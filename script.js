5686c48cf38bdeffb550d1a6efdb0dd3320e9ba6
Ziyoda0420 <mirzakirovaziyoda@gmail.com>
2025-02-15 15:09:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// function Checkage(){
+//     alert('orqa fon sariq rangda')
+// }
+
+// Checkage.style.background = 'yellow'
+
+// function Checkage() {
+//     alert('orqa fon sariq rangda');
+//     Checkage.getElementById('Checkage');
+//     body.style.background = 'yellow'
+   
+//     // header.style.background = 'yellow'
+// }
+
+
+
+// function Checkage() {
+//     alert('orqa fon sariq rangda');
+//     document.getElementById("myButton");
+//     Checkage.style.background = 'yellow'
+//     function.body.appendchild(Checkage);
+    
+// }
+
+
+
+function Checkage() {
+    alert('orqa fon sariq rangda'); 
+    
+    // Tugmachani olish
+    let body = document.getElementById("myButton"); 
+    
+    // Tugmachaning orqa fonini o'zgartirish
+    body.style.background = 'yellow';
+}
+
+
+function Check() {
+    alert('orqa fon seriy rangda'); 
+    
+    // Tugmachani olish
+    let container = document.getElementById("myBullon"); 
+    
+    // Tugmachaning orqa fonini o'zgartirish
+    container.style.background = 'grey';
+}
